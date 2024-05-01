8cbadfec7d63e458b88112d99952edfb908bafe2
kimb112 <89435497+kimb112@users.noreply.github.com>
2024-05-01 00:32:44 -0400

    add history generation feature, created background functionality, updated delete_history

---
+++ background.js
@@ -0,0 +1,131 @@
+var historyTimeout;
+chrome.runtime.onInstalled.addListener(function() {
+    console.log('Extension Installed');
+});
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    sendResponse();
+    if (message.action === 'startAddingHistory') {
+        addHistory();
+    } else if (message.action === 'stopAddingHistory') {
+        clearTimeout(historyTimeout);
+        console.log('stop adding history items')
+    }
+});
+
+function addHistory() {
+    randomInterval = Math.floor(Math.random() * 5000);
+    historyTimeout = setTimeout(function() {
+        const urls = [];
+        urls.push("https://www.cnn.com", "https://www.nbc.com", "https://www.cbs.com", "https://www.nytimes.com", "https://www.washingtonpost.com");
+        var randomIndex = Math.floor(Math.random() * 5);
+        console.log(randomInterval + " " + urls[randomIndex]);
+
+        chrome.history.addUrl({url: urls[randomIndex]}, function() {});
+
+        addHistory();
+    }, randomInterval);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// chrome.runtime.onInstalled.addListener(function() {
+//     console.log('Extension Installed');
+// });
+
+// //for listening any message which comes from runtime
+// chrome.runtime.onMessage.addListener(messageReceived);
+
+// function messageReceived(msg, sender, sendResponse) {
+// //    console.log(msg);
+//    sendResponse("received");
+//    alert(msg)
+
+//     if (message.action == 'loopFunction') {
+//         console.log("msg: " + message);
+//         loopFunction();
+//     }
+// //    addUrl();
+// }
+
+// function loopFunction() {
+//     alert('reached here')
+//     randomInterval = Math.floor(Math.random() * 5000);
+//     localStorage.setItem("randomInterval", randomInterval);
+//     // if stoprandominterval is false:
+//     setTimeout(function() {
+//         addUrl();
+//         loop();
+//     }, localStorage.getItem("randomInterval"));
+// };
+
+// function addUrl() {
+//     const urls = [];
+//     urls.push("https://www.cnn.com", "https://www.nbc.com", "https://www.cbs.com", "https://www.nytimes.com", "https://www.washingtonpost.com");
+//     var randomIndex = Math.floor(Math.random() * 5);
+//     alert(randomInterval + urls[randomIndex])
+
+//     var url = 'https://www.cnn.com'
+//     chrome.history.addUrl({url: urls[randomIndex]}, function() {});
+//     // chrome.tabs.update({url: urls[randomIndex]}, function() {}); // navigate to another tab; unable to revisit previous tab
+// }   
+
+// // function addUrl() {
+// //     const urls = [];
+// //     urls.push("https://www.cnn.com", "https://www.nbc.com", "https://www.cbs.com", "https://www.nytimes.com", "https://www.washingtonpost.com");
+// //     var randomIndex = Math.floor(Math.random() * 5);
+// //     alert(randomInterval + urls[randomIndex])
+
+// //     var url = 'https://www.cnn.com'
+// //     chrome.history.addUrl({url: urls[randomIndex]}, function() {});
+// //     // chrome.tabs.update({url: urls[randomIndex]}, function() {}); // navigate to another tab; unable to revisit previous tab
+// // }
+
+// // chrome.browserAction.onClicked.addListener(function(tab) {
+
+// //     var port = chrome.extension.connect({
+// //         name: "Sample Communication"
+// //     });
+// //     port.postMessage("Hi BackGround");
+// //     port.onMessage.addListener(function(msg) {
+// //         console.log("message recieved" + msg);
+// //     });
+
+// // });
--- script.js
+++ script.js
@@ -2,8 +2,16 @@ var startSession;
 var startTimeClick;
 
 var currentTime;
+var randomInterval;
+var stopRandomInterval;
 
-document.addEventListener("DOMContentLoaded", function() {
+// TODO: 
+// - create way to pause session so that certain items are not deleted
+// - loop through headlines (from CNN API to get headlines) to load new websites
+// - take in user input for time or keywords to delete
+// - change color of buttons based on whether session is started/stopped
+
+document.addEventListener('DOMContentLoaded', function() {
     const refreshButton = document.getElementById("refreshButton");
     // const newsTable = document.getElementById("newsTable").getElementsByTagName('tbody')[0];
 
@@ -38,37 +46,36 @@ document.addEventListener("DOMContentLoaded", function() {
     // // Add event listener to refresh button
     // refreshButton.addEventListener("click", fetchTopHeadlines);
 
-
     document.getElementById('clearHistory1hr').addEventListener('click', function() {
         currentTime = (new Date()).getTime();
-        var timeToClear = currentTime - (1 * 60 * 60* 1000); // clearing for 1 hour (div by 60 for 1 min)
+        var timeToClear = currentTime - (1 * 60 * 60 * 1000); // clearing for 1 hour (div by 60 for 1 min)
         deleteHistoryRange(timeToClear, currentTime);
         addUrl();        
     });
-    
+
     document.getElementById('clearHistory6hr').addEventListener('click', function() {
         currentTime = (new Date()).getTime();
         var timeToClear = currentTime - (6 * 60 * 60* 1000);
         deleteHistoryRange(timeToClear, currentTime);
         addUrl();        
     });
-    
+
     document.getElementById('clearHistory24hr').addEventListener('click', function() {
         currentTime = (new Date()).getTime();
         var timeToClear = currentTime - (24 * 60 * 60* 1000);
         deleteHistoryRange(timeToClear, currentTime);
         addUrl();        
     });
-    
-    
+
     document.getElementById('clearHistoryAll').addEventListener('click', function() {
         chrome.history.deleteAll();
         addUrl();        
     });
 
     document.getElementById('clearHistoryRange').addEventListener('click', function() {
+        // this was hard to figure out because we have to store variables in local storage so they can be accessed after we nagivate away from the extension (visit another page, reload)
         const isEqual = localStorage.getItem("startSession") === "true";
-        if (isEqual) { // clear history unlimited # of times until session is stopped
+        if (isEqual) {
             currentTime = (new Date()).getTime();
             deleteHistoryRange(parseInt(localStorage.getItem("startTimeClick")), currentTime);
             addUrl();
@@ -77,19 +84,13 @@ document.addEventListener("DOMContentLoaded", function() {
             alert('nothing to remove'); // must turn on session
         } 
     });
-    
+
     document.getElementById('start').addEventListener('click', function() {
         startSession = new Boolean(true); // start recording urls
         startTimeClick = (new Date()).getTime(); // when start was clicked
         localStorage.setItem("startTimeClick", startTimeClick);
         localStorage.setItem("startSession", startSession);
-        alert("start session: " + localStorage.getItem("startSession"));
-    });
-
-    document.getElementById('stop').addEventListener('click', function() {
-        startSession = new Boolean(false); // stop recording urls
-        localStorage.setItem("startSession", startSession);
-        alert("start session: " + localStorage.getItem("startSession"));
+        chrome.runtime.sendMessage({action: "startAddingHistory"}, function(response) {});
     });
 
     function deleteHistoryRange(startTime, endTime) {
@@ -97,10 +98,50 @@ document.addEventListener("DOMContentLoaded", function() {
             alert('History cleared successfully.');
         }); 
     }
+
+    document.getElementById('stop').addEventListener('click', function() {
+        //for sending a message
+        start = new Boolean(false); // stop recording urls
+        localStorage.setItem("startSession", startSession);
+        chrome.runtime.sendMessage({action: "stopAddingHistory"}, function(response) {});
+    });
+
+    document.getElementById('submitButtonTime').addEventListener('click', function() {
+        var userInputTime = document.getElementById('quantity').value;
+        currentTime = (new Date()).getTime();
+        var timeToClear = currentTime - (userInputTime * 60 * 1000); // clearing for 1 hour (div by 60 for 1 min)
+        deleteHistoryRange(timeToClear, currentTime);
+    });
+    
+    document.getElementById('submitButtonKeyword').addEventListener('click', function() {
+        var userInputKeyword = document.getElementById('keyword').value;
+        deleteHistoryItems(userInputKeyword);
+    });
+
+    function deleteHistoryRange(startTime, endTime) {
+        chrome.history.deleteRange({startTime: startTime, endTime: endTime}, function() {
+            alert('History cleared successfully.');
+        }); 
+    }
+
+    function deleteHistoryItems(keyword) {
+        chrome.history.search({text: keyword, startTime: 0, maxResults: 0}, function(historyItems) {
+            historyItems.forEach(function(historyItems) {
+                chrome.history.deleteUrl({url: historyItems.url}, function() {
+                    console.log(historyItems.url);
+                });
+            });
+        });
+    }
     
     function addUrl() {
+        const urls = [];
+        urls.push("https://www.cnn.com", "https://www.nbc.com", "https://www.cbs.com", "https://www.nytimes.com", "https://www.washingtonpost.com");
+        var randomIndex = Math.floor(Math.random() * 5);
+        alert(randomInterval + urls[randomIndex])
+
         var url = 'https://www.cnn.com'
-        chrome.history.addUrl({url: url}, function() {});
-        chrome.tabs.update({url: url}, function() {}); // navigate to another tab; unable to revisit previous tab
-    }
+        chrome.history.addUrl({url: urls[randomIndex]}, function() {});
+        // chrome.tabs.update({url: urls[randomIndex]}, function() {}); // navigate to another tab; unable to revisit previous tab
+    }   
 });
